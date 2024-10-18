636ca5b043457b5b2424e9fcc9b86d7ce7f25978
Nannita1403 <nannimagi@gmail.com>
2024-10-18 18:16:59 +0200

    okok

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,17 +1,24 @@
-import Loader from "../../components/Loader/Loader";
 import MovieCard from "../../components/PlatformParts/MovieCard/MovieCard";
 import PeticionMoviesSeries from "../../providers/VariosFetch/Peticion";
 import PreviewMovie from "../../components/PlatformParts/PreviewMovie/PreviewMovie"
+import { useFetchData } from "../../providers/FetchApiProvider/IMBFetch";
+import Loader from "../../components/Loader/Loader";
 
 
 const Home = () => {
   //const { light } = useContext(ThemeContext));
-  
+  const { movies, series } = useFetchData()
+  const handleMovieSearch = (inputValue) => {
+    setFilter(inputValue)
+  }
+
+  if (!movies.length || !series.length) {
+    return <Loader/>
+  }
   
   return (
     <>
   {/*<Loader/>*/}
-
  { /*<PreviewMovie/>*/}
 
 
